b7362e586bdf07196138a9dfe27a4d22f9f91506
asya <asyauniverse@gmail.com>
2024-03-30 01:56:04 +0300

    add request, accept, withdraw and healthfactor functions

--- contracts/lending.sol
+++ contracts/lending.sol
@@ -6,18 +6,152 @@ import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 import {IERC721} from "@openzeppelin/contracts/interfaces/IERC721.sol";
 import {IERC721Receiver} from "@openzeppelin/contracts/interfaces/IERC721Receiver.sol";
 
-contract lnedingNft is IERC721Receiver {
-    struct NftInfo {
-        uint128 id;
-        uint128 price;
+contract LendingNft is IERC721Receiver {
+    error LendingNft__NotAnOwnerOfTokenId();
+    error LendingNft__NotAnAcceptor();
+    error LendingNft__ErrorPrice();
+    error LendingNft__NotSuchNft(address token, uint256 tokenId);
+    error LendingNft__IllHealthFactor();
+
+    event UserRequest(
+        address indexed user,
+        address indexed token,
+        uint256 indexed id,
+        uint256 requestedPrice,
+        uint256 floorPrice
+    );
+
+    event Accept(uint256 indexed requestId, uint256 indexed price);
+
+    event ReceiveNft(
+        address indexed from,
+        address indexed token,
+        uint256 tokenId
+    );
+
+    event WithdrawNft(
+        address indexed to,
+        address indexed token,
+        uint256 indexed tokenId
+    );
+
+    enum RequestStatus {
+        Pending,
+        Accepted,
+        Rejected
     }
 
-    mapping(address user => mapping(address nftContract => NftInfo)) users;
+    struct Request {
+        address user;
+        address nftContract;
+        uint256 id;
+        uint256 requestedPrice;
+        uint256 floorPrice;
+        RequestStatus status;
+    }
+
+    struct Collateral {
+        uint256 deposit;
+        uint256 borrow;
+    }
+
+    uint256 private constant PRECISION = 1e18;
+    uint256 private constant LIQUIDATION_TRESHOLD = 2e18;
+
+    address private s_acceptor;
+
+    // mapping(address user => mapping(address nftContract => NftInfo)) users;
+    mapping(address user => mapping(address nftContract => mapping(uint256 id => uint256 price))) depositedNfts;
+    Request[] private s_requests;
+    mapping(address user => Collateral) s_collaterals;
+
+    constructor(address acceptor) payable {
+        s_acceptor = acceptor;
+    }
+
+    function requestDepositNft(
+        address tokenAddr,
+        uint256 tokenId,
+        uint256 _requestedPrice,
+        uint256 _floorPrice
+    ) external {
+        if (IERC721(tokenAddr).ownerOf(tokenId) != msg.sender)
+            revert LendingNft__NotAnOwnerOfTokenId();
+
+        s_requests.push(
+            Request({
+                user: msg.sender,
+                nftContract: tokenAddr,
+                id: tokenId,
+                requestedPrice: _requestedPrice,
+                floorPrice: _floorPrice,
+                status: RequestStatus.Pending
+            })
+        );
+
+        emit UserRequest(
+            msg.sender,
+            tokenAddr,
+            tokenId,
+            _requestedPrice,
+            _floorPrice
+        );
+    }
+
+    function acceptDepositAndTransferNft(
+        uint256 requestId,
+        uint256 price
+    ) external {
+        if (msg.sender != s_acceptor) revert LendingNft__NotAnAcceptor();
+        Request memory request = s_requests[requestId];
+
+        if (price < request.floorPrice) revert LendingNft__ErrorPrice();
+
+        request.status = RequestStatus.Accepted;
+        depositedNfts[request.user][request.nftContract][request.id] = price;
+        s_collaterals[request.user].deposit += price;
+
+        emit Accept(requestId, price);
+
+        IERC721(request.nftContract).safeTransferFrom(
+            request.user,
+            address(this),
+            request.id
+        );
+    }
+
+    function withdrawNft(address token, uint256 tokenId) external {
+        uint256 price = depositedNfts[msg.sender][token][tokenId];
+        if (price == 0) revert LendingNft__NotSuchNft(token, tokenId);
+
+        s_collaterals[msg.sender].deposit -= price;
+        if (checkHealthFactor(msg.sender) < LIQUIDATION_TRESHOLD)
+            revert LendingNft__IllHealthFactor();
+        delete depositedNfts[msg.sender][token][tokenId];
+
+        emit WithdrawNft(msg.sender, token, tokenId);
+
+        IERC721(token).transferFrom(address(this), msg.sender, tokenId);
+    }
+
+    function checkHealthFactor(
+        address user
+    ) public view returns (uint256 healthFactor) {
+        if (s_collaterals[user].borrow == 0) {
+            healthFactor = s_collaterals[user].deposit;
+        } else {
+            healthFactor =
+                (s_collaterals[user].deposit * PRECISION) /
+                s_collaterals[user].borrow;
+        }
+    }
 
     function onERC721Received(
         address operator,
         address from,
         uint256 tokenId,
         bytes calldata data
-    ) external returns (bytes4) {}
+    ) external returns (bytes4) {
+        emit ReceiveNft(from, msg.sender, tokenId);
+    }
 }
